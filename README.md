a3e9d45f6835845c3f95bc713cec3e6dac1ac0b7
Mark Voortman <mark@voortman.name>
2018-08-22 23:08:59 -0400

    Initial commit for the portfolio builder (derived from the syllabus builder).

---
+++ README.md
@@ -0,0 +1,15 @@
+# Portfolio Builder
+
+This is a simple web application to make it easy to collect portfolios.
+
+# Usage
+
+Copy `sample.env` to `.env`. Then replace the `YOUR APP ID HERE` with the application ID and `YOUR APP PASSWORD HERE` with the password you generated and save your changes. Replace `YOUR ADMIN EMAILS HERE` with the email addresses of your admininstrators. Only admins are allowed to edit the template. Then run `node ./bin/www` to start the web server.
+
+# Credits
+
+Some of the files in this project originated from this repository and are under the MIT license: https://github.com/jasonjoh/node-tutorial
+
+# Copyright
+
+Copyright (c) Mark Voortman - mark@voortman.name. All rights reserved.
---
+++ app.js
@@ -0,0 +1,154 @@
+// Copyright (c) Mark Voortman - mark@voortman.name. All rights reserved. Licensed under the MIT license. See LICENSE.txt in the project root for license information.
+// Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE.txt in the project root for license information.
+var fs = require("fs");
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var formidable = require("formidable");
+
+require("dotenv").config();
+
+var index = require("./routes/index");
+var authorize = require("./routes/authorize");
+var builder = require("./routes/builder");
+var template = require("./routes/template");
+
+var authHelper = require("./helpers/auth");
+
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", index);
+app.use("/authorize", authorize);
+app.use("/builder", builder);
+app.use("/template", template);
+
+app.post("/save_template", async function(req, res, next) {
+  const accessToken = await authHelper.getAccessToken(req.cookies, res);
+  const userAdmin = req.cookies.graph_user_admin;
+  if (accessToken && userAdmin) {
+    res.setHeader("Content-Type", "application/json");
+    fs.writeFile("data/template.json", req.body.data+"\n", "utf8", function(err) {
+      res.send(JSON.stringify({
+        error: err
+      }));
+    });
+  }
+});
+
+app.get("/load_template", async function(req, res, next) {
+  const accessToken = await authHelper.getAccessToken(req.cookies, res);
+  const userEmail = req.cookies.graph_user_email;
+  if (accessToken && userEmail) {
+    res.setHeader("Content-Type", "application/json");
+    fs.readFile("data/template.json", "utf8", function(err, templatestr) {
+      var tmp = err ? {} : JSON.parse(templatestr);
+      if (err && err.code !== "ENOENT") {
+        tmp.error = err;
+      }
+      res.send(tmp);
+    });
+  }
+});
+
+app.post("/save_data", async function(req, res, next) {
+  const accessToken = await authHelper.getAccessToken(req.cookies, res);
+  const userEmail = req.cookies.graph_user_email;
+  if (accessToken && userEmail) {
+    res.setHeader("Content-Type", "application/json");
+    fs.writeFile("data/" + userEmail + ".json", req.body.data+"\n", "utf8", function(err) {
+      res.send(JSON.stringify({
+        error: err
+      }));
+    });
+  }
+});
+
+app.get("/load_data", async function(req, res, next) {
+  const accessToken = await authHelper.getAccessToken(req.cookies, res);
+  const userEmail = req.cookies.graph_user_email;
+  if (accessToken && userEmail) {
+    res.setHeader("Content-Type", "application/json");
+    fs.readFile("data/" + userEmail + ".json", "utf8", function(err, templatestr) {
+      var tmp = err ? {} : JSON.parse(templatestr);
+      if (err && err.code !== "ENOENT") {
+        tmp.error = err;
+      }
+      if (!tmp.email) {
+        tmp.email = userEmail;
+      }
+      res.send(tmp);
+    });
+  }
+});
+
+app.post("/fileupload", async function(req, res, next) {
+  const accessToken = await authHelper.getAccessToken(req.cookies, res);
+  const userEmail = req.cookies.graph_user_email;
+  if (accessToken && userEmail) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files) {
+      if (err) {
+        res.setHeader("Content-Type", "application/json");
+        res.send(JSON.stringify({
+          error: err
+        }));
+      }
+      else {
+        var id = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+        fs.rename(files.file.path, "data/" + userEmail + ":" + id + ":" + files.file.name, function(err) {
+          res.setHeader("Content-Type", "application/json");
+          res.send(JSON.stringify({
+            error: err,
+            id: id,
+            name: files.file.name
+          }));
+        });
+      }
+    });
+  }
+});
+
+app.get("/files/", async function(req, res, next) {
+  const accessToken = await authHelper.getAccessToken(req.cookies, res);
+  const userEmail = req.cookies.graph_user_email;
+  if (accessToken && userEmail) {
+    console.log(req.url);
+    //res.send("data/" + userEmail + ":" + ...);
+  }
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
